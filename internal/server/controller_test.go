e744ba6ec3ced45d5b4bc2c6de3a1d4c210041a8
clouding <ghost0436@gmail.com>
2020-05-08 00:19:57 +0800

    Cleanup controller test

--- internal/server/controller_test.go
+++ internal/server/controller_test.go
@@ -7,66 +7,71 @@ import (
 	"testing"
 
 	"github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestController_GetNext(t *testing.T) {
-	t.Run("success", func(t *testing.T) {
-		u1, _ := url.Parse("http://example1.com")
-		u2, _ := url.Parse("http://example2.com")
-		u3, _ := url.Parse("http://example3.com")
-
-		c := NewController()
-		c.SetupServers(u1, u2, u3)
-
-		assert.Equal(t, u1, c.getNext().url)
-		assert.Equal(t, u2, c.getNext().url)
-		assert.Equal(t, u3, c.getNext().url)
-	})
-	t.Run("failed", func(t *testing.T) {
-		c := NewController()
-		assert.Nil(t, c.getNext())
-	})
+type ControllerTestSuite struct {
+	suite.Suite
+	u1  *url.URL
+	u2  *url.URL
+	u3  *url.URL
+	out *bytes.Buffer
 }
 
-func TestController_HealthCheck(t *testing.T) {
-	b := &bytes.Buffer{}
-	logrus.SetOutput(b)
-
+func (suite *ControllerTestSuite) SetupTest() {
 	u1, _ := url.Parse("http://localhost:1234")
 	u2, _ := url.Parse("http://localhost:1235")
 	u3, _ := url.Parse("http://localhost:1236")
+	suite.u1 = u1
+	suite.u2 = u2
+	suite.u3 = u3
 
+	suite.out = &bytes.Buffer{}
+	logrus.SetOutput(suite.out)
+}
+
+func (suite *ControllerTestSuite) TestGetNext() {
 	c := NewController()
-	c.SetupServers(u1, u2, u3)
+	c.SetupServers(suite.u1, suite.u2, suite.u3)
+
+	suite.Equal(suite.u1, c.getNext().url)
+	suite.Equal(suite.u2, c.getNext().url)
+	suite.Equal(suite.u3, c.getNext().url)
+}
+
+func (suite *ControllerTestSuite) TestGetNextFail() {
+	c := NewController()
+	suite.Nil(c.getNext())
+}
+
+func (suite *ControllerTestSuite) TestHealthCheck() {
+	c := NewController()
+	c.SetupServers(suite.u1, suite.u2, suite.u3)
 
 	c.HealthCheck()
-	assert.Equal(t, 0, c.upIDs.Len())
-	assert.Equal(t, 3, c.downIDs.Len())
+	suite.Equal(0, c.upIDs.Len())
+	suite.Equal(3, c.downIDs.Len())
 
 	ln, err := net.Listen("tcp4", "localhost:1234")
-	assert.NoError(t, err)
+	suite.NoError(err)
 
 	c.HealthCheck()
-	assert.Equal(t, 1, c.upIDs.Len())
-	assert.Equal(t, 2, c.downIDs.Len())
+	suite.Equal(1, c.upIDs.Len())
+	suite.Equal(2, c.downIDs.Len())
 
 	ln.Close()
 }
 
-func TestController_Down(t *testing.T) {
-	b := &bytes.Buffer{}
-	logrus.SetOutput(b)
-
-	u1, _ := url.Parse("http://localhost:1234")
-	u2, _ := url.Parse("http://localhost:1235")
-	u3, _ := url.Parse("http://localhost:1236")
-
+func (suite *ControllerTestSuite) TestDown() {
 	c := NewController()
-	c.SetupServers(u1, u2, u3)
+	c.SetupServers(suite.u1, suite.u2, suite.u3)
 
 	c.Down(1)
-	assert.Equal(t, 2, c.upIDs.Len())
-	assert.Equal(t, 1, c.downIDs.Len())
-	assert.Contains(t, b.String(), "[http://localhost:1234] down")
+	suite.Equal(2, c.upIDs.Len())
+	suite.Equal(1, c.downIDs.Len())
+	suite.Contains(suite.out.String(), "[http://localhost:1234] down")
+}
+
+func TestControllerTestSuite(t *testing.T) {
+	suite.Run(t, new(ControllerTestSuite))
 }
